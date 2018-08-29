5047b04d212f2f1c076156605d3da6c878307825
luotaooo <luotao0219@gmail.com>
2018-08-29 18:11:31 +0800

    icon

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -107,32 +107,22 @@ export default {
   .icons
     margin-top: .1rem
     .icon
-      position: relative
-      overflow: hidden
-      float: left
-      width: 25%
-      height: 0
-      padding-bottom: 25%
+      float: left;
+      width: 25%;
+      height: 1.5rem;
+      padding-top: .2rem;
+      text-align: center;
       .icon-img
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: .44rem
-        box-sizing: border-box
-        padding: .1rem
+        display inline-block
+        width 1.1rem
+        height 1.1rem
         .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
+          background 0
+          height: 1.1rem;
+          width: 1.1rem;
       .icon-desc
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height: .44rem
-        line-height: .44rem
-        text-align: center
+        margin-top: .1rem;
+        font-size: .28rem;
         color: $darkTextColor
         ellipsis()
 </style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" alt="">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <a class="item-button" :href="item.url">查看详情</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1505/be/be4802e10f3b3107.water.jpg_200x200_0e98aabe.jpg',
+          title: '武隆天坑地缝国家地质公园',
+          desc: '武隆天坑地缝国家地质公园',
+          url: 'www.baidu.com'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    line-height .8rem
+    background-color #eee
+    margin-top .2rem
+    text-indent .2rem
+  .item
+    display flex
+    height 1.9rem
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      min-width 0
+      flex 1
+      padding .1rem
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color #ccc
+        ellipsis()
+      .item-button
+        background #ff9300
+        line-height .44rem
+        padding 0 .2rem
+        border-radius .06rem
+        display inline-block
+        border 0
+        margin-top .2rem
+        color #fff
+        text-decoration none
+</style>
