65860e1fcd6527f27c3fab2c951a1653642a00bc
luotaooo <luotao0219@gmail.com>
2018-09-06 17:39:32 +0800

    city-list

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="list">
+      <div class="area">
+        <div class="title">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">A</div>
+        <div class="item-list">
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">B</div>
+        <div class="item-list">
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">C</div>
+        <div class="item-list">
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+          <div class="item">阿拉尔</div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/common.styl"
+  .list
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    overflow hidden
+    .title
+      line-height .4rem
+      background #eee
+      padding-left .2rem
+      color #666
+      /*border-top .02rem solid #ccc*/
+      /*border-bottom .02rem solid #ccc*/
+    .button-list
+      padding .1rem .6rem .1rem .1rem
+      overflow hidden
+      .button-wrapper
+        float: left
+        width 33.33%
+        .button
+          padding .1rem 0rem
+          margin .1rem
+          text-align center
+          border .02rem solid #ccc
+          border-radius .06rem
+    .item-list
+      .item
+        line-height .76rem
+        padding-left .2rem
+        border-bottom .01rem solid #eee
+</style>
