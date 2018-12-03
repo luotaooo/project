f7fedeb8f95b12db7f10c2c860bc0fe57e16c9eb
luotaooo <luotao0219@gmail.com>
2018-12-03 15:01:03 +0800

    finish city css

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -16,5 +16,16 @@ export default {
 <style lang="stylus" scoped>
   @import '~styles/common.styl'
   .list
-    position:
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width .4rem
+    display flex
+    flex-direction column
+    justify-content center
+    .item
+      line-height .4rem
+      text-align center
+      color $bgColor
 </style>
