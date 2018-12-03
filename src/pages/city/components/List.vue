e030e7665e928f84383087019a02bd44ad7a0ee0
luotaooo <luotao0219@gmail.com>
2018-12-03 15:20:36 +0800

    city css

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,63 +1,69 @@
 <template>
-    <div class="list">
-      <div class="area">
-        <div class="title">当前城市</div>
-        <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title">热门城市</div>
-        <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
+    <div class="list" ref="wrapper">
+      <div>
+        <div class="area">
+          <div class="title">当前城市</div>
+          <div class="button-list">
+            <div class="button-wrapper">
+              <div class="button">北京</div>
+            </div>
+            <div class="button-wrapper">
+              <div class="button">北京</div>
+            </div>
+            <div class="button-wrapper">
+              <div class="button">北京</div>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="area">
-        <div class="title">A</div>
-        <div class="item-list">
-          <div class="item">阿拉尔</div>
-          <div class="item">阿拉尔</div>
-          <div class="item">阿拉尔</div>
-          <div class="item">阿拉尔</div>
+        <div class="area">
+          <div class="title">热门城市</div>
+          <div class="button-list">
+            <div class="button-wrapper">
+              <div class="button">北京</div>
+            </div>
+            <div class="button-wrapper">
+              <div class="button">北京</div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="area">
-        <div class="title">B</div>
-        <div class="item-list">
-          <div class="item">阿拉尔</div>
-          <div class="item">阿拉尔</div>
-          <div class="item">阿拉尔</div>
-          <div class="item">阿拉尔</div>
+        <div class="area">
+          <div class="title">A</div>
+          <div class="item-list">
+            <div class="item">阿拉尔</div>
+            <div class="item">阿拉尔</div>
+            <div class="item">阿拉尔</div>
+            <div class="item">阿拉尔</div>
+          </div>
         </div>
-      </div>
-      <div class="area">
-        <div class="title">C</div>
-        <div class="item-list">
-          <div class="item">阿拉尔</div>
-          <div class="item">阿拉尔</div>
-          <div class="item">阿拉尔</div>
-          <div class="item">阿拉尔</div>
+        <div class="area">
+          <div class="title">B</div>
+          <div class="item-list">
+            <div class="item">阿拉尔</div>
+            <div class="item">阿拉尔</div>
+            <div class="item">阿拉尔</div>
+            <div class="item">阿拉尔</div>
+          </div>
+        </div>
+        <div class="area">
+          <div class="title">C</div>
+          <div class="item-list">
+            <div class="item">阿拉尔</div>
+            <div class="item">阿拉尔</div>
+            <div class="item">阿拉尔</div>
+            <div class="item">阿拉尔</div>
+          </div>
         </div>
       </div>
     </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
 }
 </script>
 
@@ -71,7 +77,7 @@ export default {
     bottom 0
     overflow hidden
     .title
-      line-height .4rem
+      line-height .54rem
       background #eee
       padding-left .2rem
       color #666
