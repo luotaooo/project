cd97eff7bca418392550ceffdbc05c4927aaacaa
luotaooo <luotao0219@gmail.com>
2018-12-03 14:47:10 +0800

    index-recommend

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader',
+  props: {
+    city: String
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/common.styl'
+  .list
+    position:
+</style>
